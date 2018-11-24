ee3e75266b4e96ac23596e812c2919d3232c1bc8
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-24 23:57:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,5 @@
  def begins_with_r(array)
-   i = 0
-   all = array.all?
-   if array[i].start_with?('r')
-    true
-     i += 1
-   else
-     false
-     i += 1
+   array.all? do |element|
+     element[0] == "r"
    end
  end
